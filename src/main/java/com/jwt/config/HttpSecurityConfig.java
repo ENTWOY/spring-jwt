6326286f5b973f7f5979c9c9691a1e7934f79544
Neiser Dennis Mamani Mamani <165808458+NeiserDennis@users.noreply.github.com>
2024-09-04 15:45:42 -0500

    Update dependencies and JWT configuration

--- src/main/java/com/jwt/config/HttpSecurityConfig.java
+++ src/main/java/com/jwt/config/HttpSecurityConfig.java
@@ -1,19 +1,106 @@
-/**
- * 
- */
 package com.jwt.config;
 
+import com.jwt.persistance.entities.util.Permission;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-/**
- * 
- */
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class HttpSecurityConfig {
+public class HttpSecurityConfig
+{
+    private final AuthenticationProvider authProvider;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthenticationEntryPoint authenticationEntryPoint;
+    private final AccessDeniedHandler accessDeniedHandler;
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
+    {
+        SecurityFilterChain filterChain = http
+                // desactiva seguridad csrf (nuestra app es stateless)
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(sessionManagement -> {
+                    // define una aplicacion sin estado (stateless)
+                    sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                })
+                // define estrategia de autenticacion (esta inyectada)
+                .authenticationProvider(authProvider)
+                // ejecuta el "jwtAuthenticationFilter" antes de que se ejecute el filtro "UsernamePasswordAuthenticationFilter"
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .authorizeHttpRequests(authRequest -> {
+                    requestMatchersBuilder(authRequest);
+                })
+                .exceptionHandling(exceptionHandler -> {
+                    exceptionHandler.authenticationEntryPoint(authenticationEntryPoint);
+                    exceptionHandler.accessDeniedHandler(accessDeniedHandler);
+                })
+                .build();
+
+        return filterChain;
+    }
+
+    // autorizacion basada en coincidencia de solicitudes HTTP usando permisos (authorities)
+    private static void requestMatchersBuilder(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry authRequest)
+    {
+        // Autorizacion de endpoints de productos
+        authRequest.requestMatchers(HttpMethod.GET, "/api/v1/products")
+                .hasAuthority(Permission.READ_ALL_PRODUCTS.name());
+
+        authRequest.requestMatchers(HttpMethod.GET, "/api/v1/products/{id}")
+                .hasAuthority(Permission.READ_ONE_PRODUCT.name());
+
+        authRequest.requestMatchers(HttpMethod.POST, "/api/v1/products")
+                .hasAuthority(Permission.CREATE_ONE_PRODUCT.name());
+
+        authRequest.requestMatchers(HttpMethod.PUT, "/api/v1/products/{id}")
+                .hasAuthority(Permission.UPDATE_ONE_PRODUCT.name());
+
+        authRequest.requestMatchers(HttpMethod.PUT, "/api/v1/products/{id}/disabled")
+                .hasAuthority(Permission.DISABLE_ONE_PRODUCT.name());
+
+
+        // Autorizacion endpoints de categories
+        authRequest.requestMatchers(HttpMethod.GET, "/api/v1/categories")
+                .hasAuthority(Permission.READ_ALL_CATEGORIES.name());
+
+        authRequest.requestMatchers(HttpMethod.GET, "/api/v1/categories/{id}")
+                .hasAuthority(Permission.READ_ONE_CATEGORY.name());
+
+        authRequest.requestMatchers(HttpMethod.POST, "/api/v1/categories")
+                .hasAuthority(Permission.CREATE_ONE_CATEGORY.name());
+
+        authRequest.requestMatchers(HttpMethod.PUT, "/api/v1/categories/{id}")
+                .hasAuthority(Permission.UPDATE_ONE_CATEGORY.name());
+
+        authRequest.requestMatchers(HttpMethod.PUT, "/api/v1/categories/{id}/disabled")
+                .hasAuthority(Permission.DISABLE_ONE_CATEGORY.name());
+
+
+        // Autorizacion perfil de usuarios
+        authRequest.requestMatchers(HttpMethod.GET, "/api/v1/auth/profile")
+                .hasAuthority(Permission.READ_MY_PROFILE.name());
+
+
+        // Autorizacion endpoints publicos: registrarse, login y validar token
+        authRequest.requestMatchers(HttpMethod.POST, "/api/v1/customers/register").permitAll();
+        authRequest.requestMatchers(HttpMethod.POST, "/api/v1/auth/authenticate").permitAll();
+        authRequest.requestMatchers(HttpMethod.GET, "/api/v1/auth/validate").permitAll();
+
+
+        // para acceder a cualquier otro endpoint del sistema, se debe estar autenticado
+        authRequest.anyRequest().authenticated();
+    }
 }
--- src/main/java/com/jwt/services/auth/JwtService.java
+++ src/main/java/com/jwt/services/auth/JwtService.java
@@ -1,11 +1,104 @@
 package com.jwt.services.auth;
 
+import com.jwt.persistance.entities.User;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class JwtService
 {
+    // constante que contiene la cantidad de minutos de duracion del token
+    @Value("${security.jwt.expiration_in_minutes}")
+    private Long EXPIRATION_IN_MINUTES;
 
+    // clave secreta con la cual se firma el token
+    @Value("${security.jwt.secret_key}")
+    private String SECRET_KEY;
+    public String generateToken(UserDetails user, Map<String, Object> extraClaims)
+    {
+        Date issuedAt = new Date(System.currentTimeMillis()); // fecha actual del sistema
+        Date expirationDate = new Date((EXPIRATION_IN_MINUTES * 60 * 1000) + issuedAt.getTime());
+
+        String jwt = Jwts.builder()
+                // agrega claims adicionales que no son obligatorios
+                .setClaims(extraClaims)
+                // agregar username al jwt
+                .setSubject(user.getUsername())
+                // indica fecha de emision del token
+                .setIssuedAt(issuedAt)
+                // indica fecha de expiracion del token
+                .setExpiration(expirationDate)
+                // especifica tipo de token ' "typ/JWT" '
+                .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
+                // especifica clave de firma del token y algoritmo
+                .signWith(generateKey(), SignatureAlgorithm.HS256)
+                // construye el token y retorna string equivalente al jwt
+                .compact();
+
+        return jwt;
+    }
+
+    private Key generateKey()
+    {
+        // arreglo de bytes que contiene la clave secreta
+        // byte[] keyBytes = SECRET_KEY.getBytes();
+
+        // esto lo ocupamos cuando tenemos una clave que fue codificada en base 64
+        byte[] decodedKey = Decoders.BASE64.decode(SECRET_KEY);
+
+        // genera instancia de SecretKey con algoritmo HMAC-SHA
+        return Keys.hmacShaKeyFor(decodedKey);
+    }
+
+    public String extractUsername(String jwt)
+    {
+        // extrae todos los claims del token, y luego se obtiene el claim "subject" que contiene el username
+        return extractAllClaims(jwt).getSubject();
+    }
+
+    private Claims extractAllClaims(String jwt)
+    {
+        return Jwts.parserBuilder()
+                // establece la clave de firma para verificar la autenticidad del token
+                .setSigningKey(generateKey())
+                .build()
+                .parseClaimsJws(jwt)
+                // obtiene el payload del token (la data)
+                .getBody();
+    }
+
+    public String extractJwtFromRequest(HttpServletRequest request)
+    {
+        // 1. Obtener encabezado http "Authorization", que es quien contiene el token en el request
+        String authorizationHeader = request.getHeader("Authorization");
+
+        // 1.1 Validar que header contenga texto, es decir, que venga el token en el header
+        // tambien valida que el header comience con "Bearer ", posterior a ese viene el token
+        if (!StringUtils.hasText(authorizationHeader) || !authorizationHeader.startsWith("Bearer ")) {
+
+            // si header no tiene texto y no contiene el token, retornamos un null
+            return null;
+        }
+
+        // 2. Obtiene token desde el header authorization y lo retorna
+        return authorizationHeader.split(" ")[1];
+    }
+
+    public Date extractExpirationDate(String jwt)
+    {
+        return extractAllClaims(jwt).getExpiration();
+    }
 }
